3c05d25bdd886c2c77ffd316811ea47637ed75a5
fiona-guerin <fionaestherguerin@gmail.com>
2024-06-17 04:55:11 +0200

    Logging

--- yoga_bot/call_model/call_model.py
+++ yoga_bot/call_model/call_model.py
@@ -1,3 +1,4 @@
+from concurrent.futures import ThreadPoolExecutor
 from config import config, token
 from redis import Redis
 from transformers import AutoTokenizer, AutoModelForCausalLM
@@ -7,24 +8,24 @@ import json
 
 redis = Redis(host='redis', port=6379, decode_responses=True)
 
-#if not redis.exists('yoga_flows'):
-redis.set('yoga_flows', json.dumps({
-    'Vinyasa Yoga': [config.VINYASA_EXAMPLE],
-    'Yin Yoga': [config.YIN_EXAMPLE],
-    'Power Yoga': [config.POWER_EXAMPLE],
-}))
-#if not redis.exists('next_yoga_flows'):
-redis.set('next_yoga_flows', json.dumps({
-    'Vinyasa Yoga': 0,
-    'Yin Yoga': 0,
-    'Power Yoga': 0,
-}))
-#if not redis.exists('yoga_request_tracker'):
-redis.set('yoga_request_tracker', json.dumps({
-    'Vinyasa Yoga': 0,
-    'Yin Yoga': 0,
-    'Power Yoga': 0,
-}))
+if not redis.exists('yoga_flows'):
+    redis.set('yoga_flows', json.dumps({
+        'Vinyasa Yoga': [config.VINYASA_EXAMPLE],
+        'Yin Yoga': [config.YIN_EXAMPLE],
+        'Power Yoga': [config.POWER_EXAMPLE],
+    }))
+if not redis.exists('next_yoga_flows'):
+    redis.set('next_yoga_flows', json.dumps({
+        'Vinyasa Yoga': 0,
+        'Yin Yoga': 0,
+        'Power Yoga': 0,
+    }))
+if not redis.exists('yoga_request_tracker'):
+    redis.set('yoga_request_tracker', json.dumps({
+        'Vinyasa Yoga': 0,
+        'Yin Yoga': 0,
+        'Power Yoga': 0,
+    }))
 
 if False: # torch.backends.mps.is_available():
     print('mps is used.')
@@ -33,7 +34,7 @@ elif torch.cuda.is_available():
     print('cuda is used.')
     device_name = 'cuda'
 else:
-    print('cpu is used.')
+    print('CPU is used.')
     device_name = 'cpu'
 device = torch.device(device_name)
 
@@ -41,27 +42,30 @@ tokenizer = AutoTokenizer.from_pretrained(config.MODEL_NAME, token=token.HF_TOKE
 model = AutoModelForCausalLM.from_pretrained(config.MODEL_NAME, token=token.HF_TOKEN)
 model.to(device)
 
+def call_model():
+    while True:
+        yoga_type = ''
+        yoga_request_tracker = json.loads(redis.get('yoga_request_tracker'))
+        for yoga_request_type, yoga_request_count in yoga_request_tracker.items():
+            if int(yoga_request_count) > 0:
+                yoga_type = yoga_request_type
+        if not yoga_type:
+            print('No new yoga requests. Going to sleep ...')
+            time.sleep(10)
+        else:
+            print(f'Generating a novel {yoga_type} flow.')
+            prompt = f'{config.PROMPT}{yoga_type}'
+            input_ids = tokenizer(prompt, return_tensors='pt').to(device_name)
+            outputs = model.generate(
+                **input_ids,
+                max_new_tokens=1000,
+            )
+            response_text = tokenizer.batch_decode(outputs[:, input_ids['input_ids'].shape[0]:])[0]
+            yoga_flows = json.loads(redis.get('yoga_flows'))
+            yoga_flows[yoga_type].append(response_text)
+            
+            redis.set('yoga_flows', json.dumps(yoga_flows))
+            print(response_text)
 
-while True:
-    yoga_type = ''
-    yoga_request_tracker = json.loads(redis.get('yoga_request_tracker'))
-    for yoga_request_type, yoga_request_count in yoga_request_tracker.items():
-        if int(yoga_request_count) > 0:
-            yoga_type = yoga_request_type
-    if not yoga_type:
-        print('No new yoga requests. Going to sleep ...')
-        time.sleep(10)
-    else:
-        print(f'Generating a novel {yoga_type} flow.')
-        prompt = f'{config.PROMPT}{yoga_type}'
-        input_ids = tokenizer(prompt, return_tensors='pt').to(device_name)
-        outputs = model.generate(
-            **input_ids,
-            max_new_tokens=1000,
-        )
-        response_text = tokenizer.batch_decode(outputs[:, input_ids['input_ids'].shape[0]:])[0]
-        yoga_flows = json.loads(redis.get('yoga_flows'))
-        yoga_flows[yoga_type].append(response_text)
-        
-        redis.set('yoga_flows', json.dumps(yoga_flows))
-        print(response_text)
+with ThreadPoolExecutor(max_workers=1) as executor:
+    executor.submit(call_model)
